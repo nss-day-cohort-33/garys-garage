d1c27c2587e74c488c03316f47644febfab0ee9d
Steve Brownlee <chortlehoort@gmail.com>
2019-09-03 11:58:24 -0500

    Initial commit

---
+++ vehicle.py
@@ -0,0 +1,8 @@
+class Vehicle:
+
+    def __init__(self, model, manufacturer, hp, wheels):
+        self.model = model
+        self.manufacturer = manufacturer
+        self.horsepower = hp
+        self.wheel_count = wheels
+
